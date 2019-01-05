fe56910409ab5678944c37f77c8777fd028bfd6e
Rohit <rohitdeshmukh69@gmail.com>
2019-01-05 20:36:10 +0530

    added basic _make_request, _build_url and _encode_parameters

--- example.py
+++ example.py
@@ -3,7 +3,8 @@
 import postcodes_io
 
 
-api = postcodes_io.Api()
+api = postcodes_io.Api(debug_http=True)
+
 
 print(api.validate_postcode('SW'))
 print(api.validate_postcode('SW112ef'))
--- postcodes_io/api.py
+++ postcodes_io/api.py
@@ -1,6 +1,13 @@
 
 import requests
 import logging
+import json
+
+
+#TODO: could add support for python 2
+from urllib.parse import urlparse, urlunparse, urlencode, quote_plus
+from urllib.request import __version__ as urllib_version
+
 
 logger = logging.getLogger(__name__)
 
@@ -39,9 +46,58 @@ class Api(object):
     def validate_postcode(self,postcode):
         """
         This method validates post_code
+        :param postcode: postcode to check i.e. 'SW112EF'
         """
         print("calling validate_postcode method")
-        if len(postcode) > 4:
-            return True
-        else:
-            return False
+        #if len(postcode) > 4:
+        #    return True
+        #else:
+        #    return False
+
+
+    def _make_request(self, http_method, url, data=None, json=None):
+        """
+
+        :param http_method: http method i.e. GET, POST, PUT etc
+        :param url: api endpoint url
+        :param data: dictionary of key/value params
+        :param json:
+        :return:
+        """
+        if not data:
+            data = {}
+
+        response = 0
+        if http_method == 'GET':
+            url = self._build_url(url, extra_params=data)
+            response = self._session.get(url,timeout=self._timeout)
+        elif http_method == 'POST':
+            if data:
+                response = self._session.post(url, data=data, timeout=self._timeout)
+            elif json:
+                response = self._session.post(url, json=json, timeout=self._timeout)
+
+        return response
+
+    def _build_url(self, url, path_elements=None, extra_params=None):
+        (scheme, netloc, path, params, query, fragment) = urlparse(url)
+
+        # Add extra_parameters to the query
+        if extra_params and len(extra_params) > 0:
+            extra_query = self._encode_parameters(extra_params)
+            if query:
+                query += '&' + extra_query
+            else:
+                query = extra_query
+        return urlunparse((scheme, netloc, path, params, query, fragment))
+
+    @staticmethod
+    def _encode_parameters(parameters):
+        """
+        Return url encoded string in 'key=value&key=value' format
+        """
+        if not parameters or not isinstance(parameters, dict):
+            return None
+
+        return urlencode(parameters)
+
