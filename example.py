1387ae9bf53b583028c3715b50631d7f15d57914
Rohit <rohitdeshmukh69@gmail.com>
2019-01-05 23:23:01 +0530

    udpated

--- example.py
+++ example.py
@@ -22,6 +22,12 @@ data = api.get_postcodes_for_coordinates(latitude=lat,longitude=lon,limit=2)
 print(data)
 """
 
+#data = api.get_bulk_postcodes({"postcodes":["OX49 5NU","M32 0JG","NE30 1DP"]})
+list = ["SW112EF"]
+data = api.get_bulk_postcodes(list)
+print("\n")
+print(data)
+
 """
 if api.validate_postcode(postcode1):
     print("postcode ="+ postcode1+ " is valid")
