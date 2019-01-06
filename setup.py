1b814d3f6f13137e0409c21796c3562087e9830e
Rohit <rohitdeshmukh69@gmail.com>
2019-01-06 23:32:35 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -10,9 +10,14 @@ setuptools.setup(
     author_email="raigad1630@gmail.com",
     description="A Python Wrapper for postcodes.io",
     long_description=long_description,
+    license='MIT',
+    keywords='uk postcode geocode coordinates latitude longitude postcodesio royalmail',
+    python_requires='>=3',
     url="https://github.com/raigad/python-postcodes-io",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
     classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
