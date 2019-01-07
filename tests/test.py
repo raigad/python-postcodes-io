c01144cab612b87348cfb8edf70f34bdcfea023f
Rohit <rohitdeshmukh69@gmail.com>
2019-01-07 16:38:10 +0530

    added new tests

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,8 @@ import random
 class PostcodeIOTest(unittest.TestCase):
     def setUp(self):
         self.api_client = postcodes_io.Api()
+        self.latitude = 51.466324
+        self.longitude = -0.173606
         self.VALID_POSTCODES = ['L40TH', 'SW151JF', 'SW195AG', 'KT185LQ', 'NW87JY', 'RG11LZ']
         self.INVALID_POSTCODES = ['HA9997QP', 'SW19OSZ', 'HA899NX', 'RG101LZ', 'SW12EF', 'L500QD']
         self.TERMINATED_POSTCODES = ['SW112ZW', 'HA89NX', 'IV23EH', 'BA12QT', 'OX13LA', 'SE109DB']
@@ -23,11 +25,26 @@ class PostcodeIOTest(unittest.TestCase):
 
     def test_get_postcode(self):
         valid_data = self.api_client.get_postcode(random.choice(self.VALID_POSTCODES))
-        self.assertEqual(200, valid_data.get('status', None), "Checking get postcode")
+        self.assertEqual(200, valid_data.get('status', None), "Checking for valid postcode")
         invalid_data = self.api_client.get_postcode(random.choice(self.INVALID_POSTCODES))
-        self.assertEqual(404, invalid_data.get('status', None), "Checking invalid postcode")
+        self.assertEqual(404, invalid_data.get('status', None), "Checking for invalid postcode")
         terminated_data = self.api_client.get_postcode(random.choice(self.TERMINATED_POSTCODES))
-        self.assertEqual(404, terminated_data.get('status', None), "Checking terminated postcode")
+        self.assertEqual(404, terminated_data.get('status', None), "Checking for terminated postcode")
+
+    def test_get_nearest_postcodes_for_postcode(self):
+        data = self.api_client.get_nearest_postcodes_for_postcode(postcode=random.choice(self.VALID_POSTCODES), limit=2)
+        self.assertEqual(200, data.get('status', None), "Checking for valid postcode")
+
+    def test_get_nearest_postcodes_for_coordinates(self):
+        data = self.api_client.get_nearest_postcodes_for_coordinates(latitude=self.latitude, longitude=self.longitude,
+                                                                     limit=3)
+        self.assertEqual(200, data.get('status', None))
+
+    def test_get_bulk_postcodes(self):
+        bulk_postcodes = [random.choice(self.VALID_POSTCODES), random.choice(self.VALID_POSTCODES)]
+        data = self.api_client.get_bulk_postcodes(bulk_postcodes)
+        self.assertEqual(200, data.get('status', None), "Checking for valid postcode")
+        self.assertEqual(2, len(data.get('result', [])), "Checking for result count")
 
     def tearDown(self):
         """
